0f6b720a5448a0a4764601a5b99cf9debaa9fa64
PierreSQS <nomo_messina@web.de>
2022-07-17 22:31:11 +0200

    Sect10_Chap79-Update Author-NOK: related code

--- src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
+++ src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
@@ -11,10 +11,15 @@ import org.springframework.stereotype.Component;
 public class AuthorDaoImpl implements AuthorDao {
     public static final String SELECT_AUTHOR_BY_ID =
             "SELECT * FROM AUTHOR where id = ?";
+
     public static final String SELECT_AUTHOR_BY_NAME =
             "SELECT * FROM AUTHOR where first_name = ? AND last_name = ?";
+
     public static final String INSERT_INTO_AUTHOR_FIRST_NAME_LAST_NAME =
             "INSERT INTO AUTHOR (first_name, last_name) VALUES (?, ?)";
+
+    public static final String UPDATE_AUTHOR_SET_FIRST_NAME_LAST_NAME_WHERE_ID =
+            "UPDATE AUTHOR SET first_name = ? , last_name = ? WHERE id = ?";
 
     private final JdbcTemplate jdbcTemplate;
 
@@ -25,13 +30,13 @@ public class AuthorDaoImpl implements AuthorDao {
     @Override
     public Author findAuthorById(Long id) {
         return jdbcTemplate.queryForObject(
-                SELECT_AUTHOR_BY_ID,AuthorMapper.authorRowMapper,id);
+                SELECT_AUTHOR_BY_ID, AuthorMapper.authorRowMapper, id);
     }
 
     @Override
     public Author findAuthorByName(String firstName, String lastName) {
         return jdbcTemplate.queryForObject(
-                SELECT_AUTHOR_BY_NAME,AuthorMapper.authorRowMapper,firstName,lastName);
+                SELECT_AUTHOR_BY_NAME, AuthorMapper.authorRowMapper, firstName, lastName);
     }
 
     @Override
@@ -40,12 +45,25 @@ public class AuthorDaoImpl implements AuthorDao {
                 INSERT_INTO_AUTHOR_FIRST_NAME_LAST_NAME,
                 author.getFirstName(), author.getLastName());
 
-        return findAuthorById((long) updatedRowID);
+        if (updatedRowID != 0) {
+            return author;
+        } else {
+            return null;
+        }
+
     }
 
     @Override
     public Author updateAuthor(Author author) {
-        return null;
+        int updatedRowID = this.jdbcTemplate.update(
+                UPDATE_AUTHOR_SET_FIRST_NAME_LAST_NAME_WHERE_ID,
+                author.getFirstName(), author.getLastName(), author.getId());
+
+        if (updatedRowID != 0) {
+            return author;
+        } else {
+            return null;
+        }
     }
 
     @Override

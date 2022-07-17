d4b453308ebeb1e81433bc2c8b8165e6a4e99ea0
PierreSQS <nomo_messina@web.de>
2022-07-17 17:27:27 +0200

    Sect10_Assn8-Create Author DAO: implemented the tests

--- src/test/java/guru/springframework/jdbc/DaoIntegrationTest.java
+++ src/test/java/guru/springframework/jdbc/DaoIntegrationTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
 import org.springframework.test.context.ActiveProfiles;
@@ -22,29 +21,64 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ActiveProfiles("local")
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import(AuthorDaoImpl.class)
 public class DaoIntegrationTest {
+
+    @Autowired
+    AuthorDao authorDao;
 
     @Test
     void testDeleteAuthor() {
+        Author author = new Author();
+        author.setFirstName("john");
+        author.setLastName("t");
 
+        Author saved = authorDao.saveNewAuthor(author);
+
+        authorDao.deleteAuthorById(saved.getId());
+
+        Author deleted = authorDao.findAuthorById(saved.getId());
+
+        assertThat(deleted).isNull();
     }
 
     @Test
     void testUpdateAuthor() {
+        Author author = new Author();
+        author.setFirstName("john");
+        author.setLastName("t");
 
+        Author saved = authorDao.saveNewAuthor(author);
+
+        saved.setLastName("Thompson");
+        Author updated = authorDao.updateAuthor(saved);
+
+        assertThat(updated.getLastName()).isEqualTo("Thompson");
     }
 
     @Test
     void testInsertAuthor() {
+        Author author = new Author();
+        author.setFirstName("john");
+        author.setLastName("t");
 
+        Author saved = authorDao.saveNewAuthor(author);
+
+        assertThat(saved).isNotNull();
     }
 
     @Test
     void testGetAuthorByName() {
+        Author author = authorDao.findAuthorByName("Craig", "Walls");
 
+        assertThat(author).isNotNull();
     }
 
     @Test
-    void deleteAuthorById() {
+    void testGetAuthor() {
+
+        Author author = authorDao.findAuthorById(1L);
+
+        assertThat(author.getId()).isNotNull();
     }
 }
